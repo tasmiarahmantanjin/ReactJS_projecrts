5373500341aabef144432da721e9a5920c87fc03
Tasmia Rahman <tasmiata@gmail.com>
2021-01-16 19:46:45 +0200

    first app done

---
+++ first_app/src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+import Button from "./Button"
+
+
+const App = () => {
+	return (
+	  <div>
+		<h1 style={{ textAlign: "center" }}> Tasmia Rahman Tanjin </h1>
+		<Button title="App Store"></Button>
+		<Button title="App Value"></Button>
+	  </div>
+	
+	);
+  }
+
+  export default App;
