7a2da26a17e24ca3200376a57892dbb7de8b5a7e
Tasmia Rahman <tasmiata@gmail.com>
2021-01-17 19:17:24 +0200

    state calculator ddone

--- three-state_counter/src/App.js
+++ three-state_counter/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import "./App.css";
 
 
@@ -6,18 +6,17 @@ const App = () => {
 	const [count, setCount] = useState(0)
 
 
-	return(
+	return (
 		<div className="App">
 			<header>
-			<h1>Counter App using state</h1>
+				<h1>Counter App using state</h1>
 			</header>
-			<h2>Current calue of count is {count}</h2>
+			<h2>Current value of count is {count}</h2>
 			<button onClick={() => setCount(0)}>Reset Button</button>
-			<button onClick={() => setCount(count + 1)}>Increase Button</button>
-			<button onClick={() => setCount(count - 1)}>Normal Button</button>
-			
+			{/* <button onClick={() => setCount(count + 1)}>Increase Button</button> */}
+			<button onClick={() => count > 10 ? "" : setCount(count + 1)}>Increase Button</button>
 
-			
+			<button onClick={() => count <= 0 ? "" : setCount(count - 1)}>Normal Button</button>
 		</div>
 	)
 }
