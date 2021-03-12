b22374e23c306633974fb1f6d107573ee1787545
Tasmia Rahman <tasmiata@gmail.com>
2021-03-12 21:43:20 +0200

    update

---
+++ todo-app_local_storage/src/componets/TodoForms.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { 
+	FormGroup,
+	Input,
+	InputGroup,
+	InputGroupAddon,
+	Button,
+	Form,
+	Container
+ } from "reactstrap";
+ import { v4 } from "uuid";
+
+ const TodoForm = () => {
+	 const [todoString, setTodoString] = useState("")
+	 return(
+		<Form>
+		<FormGroup>
+			<InputGroup>
+			<Input name="todo" type="text" id="todo" placeholder="Enter a todo Task" value={todoString} onChange={e => setTodoString(e.target.value)}/>
+			<InputGroupAddon addonType="prepend">
+			<Button color="success">Add Todo</Button>
+			</InputGroupAddon>
+			</InputGroup>
+		</FormGroup>
+	</Form>
+	 )
+ }
+
+ export default TodoForm;
